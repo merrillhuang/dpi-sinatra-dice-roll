c43080cb7486dc27ffcc17d9e6fa2482b863e52d
merrillhuang <127697576+merrillhuang@users.noreply.github.com>
2024-07-11 20:05:43 +0000

    use instructor's dice file

--- dice.rb
+++ dice.rb
@@ -2,35 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "better_errors"
 require "binding_of_caller"
-
+  
+# Need this configuration for better_errors
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "  
-  <style>
-      .navbar {
-        display: flex;
-      	justify-content: space-around;
-      }
-    </style>
-
-  <div class='navbar'>
-      <div><a href='/'>Home</a></div>
-      <div><a href='/dice/2/6'>2d6</a></div>
-      <div><a href='/dice/2/10'>2d10</a></div>
-      <div><a href='/dice/1/20'>1d20</a></div>
-      <div><a href='/dice/5/4'>5d4</a></div>
-    </div>
-
+  "
   <h1>Dice Roll</h1>
-    <ul>
-      <li><a href= '/dice/2/6'> Roll two 6-sided dice</a></li>
-      <li><a href= '/dice/2/10'>Roll two 10-sided dice</a></li>
-      <li><a href= '/dice/1/20'>Roll one 20-sided die</a></li>
-      <li><a href= '/dice/5/4'>Roll five 4-sided dice</a></li>
-    </ul>"
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
 end
 
 get("/zebra") do
@@ -43,107 +30,44 @@ end
 
 get("/dice/2/6") do
   first_die = rand(1..6)
-  second_die = rand(1..6)
+	second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "
-  <style>
-      .navbar {
-        display: flex;
-      	justify-content: space-around;
-      }
-    </style>
-
-  <div class='navbar'>
-      <div><a href='/'>Home</a></div>
-      <div><a href='/dice/2/6'>2d6</a></div>
-      <div><a href='/dice/2/10'>2d10</a></div>
-      <div><a href='/dice/1/20'>1d20</a></div>
-      <div><a href='/dice/5/4'>5d4</a></div>
-    </div>
-  <h1>2d6</h1>
+	"<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
-  second_die = rand(1..10)
+	second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "
-  <style>
-      .navbar {
-        display: flex;
-      	justify-content: space-around;
-      }
-    </style>
-
-  <div class='navbar'>
-      <div><a href='/'>Home</a></div>
-      <div><a href='/dice/2/6'>2d6</a></div>
-      <div><a href='/dice/2/10'>2d10</a></div>
-      <div><a href='/dice/1/20'>1d20</a></div>
-      <div><a href='/dice/5/4'>5d4</a></div>
-    </div>
-
-  <h1>2d10</h1>
+	"<h1>2d10</h1>
    <p>#{outcome}</p>"
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
-
-  outcome = "You rolled a #{die}."
 	
-  "
-  <style>
-      .navbar {
-        display: flex;
-      	justify-content: space-around;
-      }
-    </style>
-
-  <div class='navbar'>
-      <div><a href='/'>Home</a></div>
-      <div><a href='/dice/2/6'>2d6</a></div>
-      <div><a href='/dice/2/10'>2d10</a></div>
-      <div><a href='/dice/1/20'>1d20</a></div>
-      <div><a href='/dice/5/4'>5d4</a></div>
-    </div>
-
-  <h1>1d20</h1>
+	outcome = "You rolled a #{die}."
+	
+	"<h1>1d20</h1>
    <p>#{outcome}</p>"
 end
 
 get("/dice/5/4") do
   first_die = rand(1..4)
-  second_die = rand(1..4)
+	second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-  "
-  <style>
-      .navbar {
-        display: flex;
-      	justify-content: space-around;
-      }
-    </style>
-
-  <div class='navbar'>
-      <div><a href='/'>Home</a></div>
-      <div><a href='/dice/2/6'>2d6</a></div>
-      <div><a href='/dice/2/10'>2d10</a></div>
-      <div><a href='/dice/1/20'>1d20</a></div>
-      <div><a href='/dice/5/4'>5d4</a></div>
-    </div>
-
-  <h1>5d4</h1>
+	"<h1>5d4</h1>
    <p>#{outcome}</p>"
 end
